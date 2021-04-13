36b89b57b0d96f50103dcffcfd4cb3b1c9e07ac5
Jornwer <betanovdmitry946@gmail.com>
2021-04-13 18:52:39 +0300

    Initial commit

---
+++ src/main/java/com/jornwer/tictactoeweb/TictactoewebApplication.java
@@ -0,0 +1,13 @@
+package com.jornwer.tictactoeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jornwer/tictactoeweb/TictactoewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jornwer.tictactoeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
